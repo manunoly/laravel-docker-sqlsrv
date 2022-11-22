119132eb761dab259a1b1573dc5c7505e1aa2c65
Manuel Almaguer <manuel@entourageyearbooks.com>
2022-11-22 16:01:08 -0500

    routing for test mssql with user table

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/mssql', function () {
+    $results = DB::select('select * from users');
+    return $results;
+});
+
+//test mssql connection
+Route::get('/mssql/sql', function () {
+    $connection = DB::connection('sqlsrv');
+    $results = $connection->select("SELECT * FROM [dbo].[users]");
+    return $results;
 });
