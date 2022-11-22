124c9712f5b8af125cdafffc0f14520d135cc8fb
Manuel Almaguer <manuel@entourageyearbooks.com>
2022-11-22 15:57:26 -0500

    seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Manuel Almaguer',
+            'email' => 'manuel@entourageyearbooks.com',
+            'password' => bcrypt('manuel123'),
+
+        ]);
     }
 }
