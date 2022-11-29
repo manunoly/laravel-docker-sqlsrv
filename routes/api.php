4ccf58d1dedfd2e359f46a5475342075ea6ced39
Manuel Almaguer <manuel@entourageyearbooks.com>
2022-11-29 15:12:40 -0500

    working api with breeze for nextjs

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,69 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
-Route::get('/mssql', function () {
-    //get users from sqlsrv using eloquent
-    $users = User::on('sqlsrv')->limit(11)->get();
-    return $users;
-});
-
-Route::get('/users/insert/{cxn?}', function ($cnx = 'mysql') {
-    $name = fake()->name();
-    $email = fake()->unique()->safeEmail();
-    $email = preg_split('/@/', $email)[0];
-    $startDate = date('Y-m-d H:i:s');
-    foreach (range(1, 100000) as $index) {
-        $user = new User();
-        $user->name = $name;
-        $user->email = $email . $index . '@gmail.com';
-        $user->password = 'test123';
-        $user->setConnection($cnx);
-        $user->save();
-        $index++;
-    }
-    $endDate = date('Y-m-d H:i:s');
-    $time = strtotime($endDate) - strtotime($startDate);
-    $message = 'Time to insert 100000 users: ' . $time . ' seconds' . ' using ' . $cnx . ' connection';
-    Log::info($message);
-    return $message;
-});
-
-Route::get('/users/bulk-insert/{cxn?}', function ($cnx = 'mysql') {
-    $name = fake()->name();
-    $email = fake()->unique()->safeEmail();
-    $email = preg_split('/@/', $email)[0];
-    $startDate = date('Y-m-d H:i:s');
-    $users = [];
-    foreach (range(1, 10000) as $index) {
-        $users[] = [
-            'name' => $name,
-            'email' => $email . $index . '@gmail.com',
-            'password' => 'test123',
-        ];
-        $index++;
-        if ($index % 500 == 0) {
-            // both works
-            // DB::connection($cnx)->table('users')->insert($users);
-            User::on($cnx)->insert($users);
-            $users = [];
-        }
-    }
-    $endDate = date('Y-m-d H:i:s');
-    $time = strtotime($endDate) - strtotime($startDate);
-    $message = 'Time to bulk insert 1000000 users: ' . $time . ' seconds';
-    return $message;
-});
-
-
-Route::get('/users/{cxn?}', function ($cnx = 'mysql') {
-    $users = User::on($cnx)->limit(11)->get();
-    return $users;
 });
